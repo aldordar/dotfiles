f8088bcae895fbb887ffe1c934083193bb0aa683
Alberto Dorta Darias <aldo@html24.net>
2020-03-17 12:11:54 +0000

    New Dev Apps for Linux

--- brew_apps.sh
+++ brew_apps.sh
@@ -2,33 +2,37 @@
 
 # Brew Packages
 
-brew install tree
-#brew install vim
-brew install git
-brew install miniserve
-brew install htop
+brew install tree 
+#brew install vim 
+brew install git 
+brew install miniserve 
+brew install htop 
 brew install ffmpeg
-brew install node
-brew install tmux
+brew install node 
+brew install tmux 
 
 # MacOS Applications
 
-brew cask install google-chrome
+brew cask install google-chrome 
 brew cask install spotify
-brew cask install discord
-brew cask install skype
-brew cask install postman
+brew cask install discord 
+brew cask install skype 
+brew cask install postman 
 brew cask install docker
-brew cask install gimp
-brew cask install vlc
+brew cask install gimp 
+brew cask install vlc 
 #brew cask install virtualbox
 brew cask install mamp
-brew cask install enpass
+brew cask install cyberduck
+brew cask install filezilla
+brew cask install enpass 
 brew cask install spectacle
 brew cask install appcleaner
 #brew cask install wireshark
-#brew cask install slack
+brew cask install slack 
 #brew cask install sourcetree
 brew cask install atom
 brew cask install android-file-transfer
-brew cask install visual-studio-code
+brew cask install visual-studio-code 
+#Install VPN Service
+#Install xclip for Linux
